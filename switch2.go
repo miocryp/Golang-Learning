8b92292fd4859ee9997edb57e45e776abff17e99
miocryp <mio.cryp@gmail.com>
2022-06-23 01:06:38 +0200

    first commit

---
+++ switch2.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	var number int = 30
+	switch number {
+	case 10:
+		println("Running case 10")
+	case 20:
+		println("Running case 20")
+	case 30:
+		println("Running case 30")
+	default:
+		println("Running default code")
+	}
+}
