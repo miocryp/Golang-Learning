ba9600ad5563f19851591a029166afe4bfb56f93
miocryp <mio.cryp@gmail.com>
2022-06-23 00:20:23 +0200

    first commit

---
+++ assignmentOperator.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	var a, b, c int
+	a, b, c = 100, 200, 300
+	var d, e, f = 400, 500, 600
+	s1, s2, s3 := "Go in 8 Hours", "is", "good book."
+	println(a, b, c)
+	println(d, e, f)
+	println(s1, s2, s3)
+}
